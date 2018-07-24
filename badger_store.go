19fe5d51aae7af867ee2811c80825b293b6ca7d2
Alvaro Alda <alvaro.alda@bbva.com>
2018-07-24 10:09:40 +0200

    Implement LogStore and StableStore interfaces

---
+++ badger_store.go
@@ -0,0 +1,258 @@
+/*
+   Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package raftbadger
+
+import (
+	"errors"
+
+	"github.com/dgraph-io/badger"
+	"github.com/hashicorp/raft"
+)
+
+var (
+	// ErrKeyNotFound is an error indicating a given key does not exist
+	ErrKeyNotFound = errors.New("not found")
+)
+
+// BadgerStore provides access to Badger for Raft to store and retrieve
+// log entries. It also provides key/value storage, and can be used as
+// a LogStore and StableStore.
+type BadgerStore struct {
+	// conn is the underlying handle to the db.
+	conn *badger.DB
+
+	// The path to the Badger database directory.
+	path string
+}
+
+// Options contains all the configuration used to open the Badger db
+type Options struct {
+	// Path is the directory path to the Badger db to use.
+	Path string
+
+	// BadgerOptions contains any specific Badger options you might
+	// want to specify.
+	BadgerOptions *badger.Options
+
+	// NoSync causes the database to skip fsync calls after each
+	// write to the log. This is unsafe, so it should be used
+	// with caution.
+	NoSync bool
+}
+
+// NewBadgerStore takes a file path and returns a connected Raft backend.
+func NewBadgerStore(path string) (*BadgerStore, error) {
+	return New(Options{Path: path})
+}
+
+// func NewDefaultStableStore(path string) (*BadgerStore, error) {
+// 	opts := badger.DefaultOptions
+// 	opts.MaxLevels = 2
+// 	return New(Options{Path: path, BadgerOptions: &opts})
+// }
+
+// New uses the supplied options to open the Badger db and prepare it for
+// use as a raft backend.
+func New(options Options) (*BadgerStore, error) {
+
+	// build badger options
+	if options.BadgerOptions == nil {
+		defaultOpts := badger.DefaultOptions
+		options.BadgerOptions = &defaultOpts
+	}
+	options.BadgerOptions.Dir = options.Path
+	options.BadgerOptions.ValueDir = options.Path
+	options.BadgerOptions.SyncWrites = !options.NoSync
+
+	// Try to connect
+	handle, err := badger.Open(*options.BadgerOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the new store
+	store := &BadgerStore{
+		conn: handle,
+		path: options.Path,
+	}
+
+	return store, nil
+}
+
+// Close is used to gracefully close the DB connection.
+func (b *BadgerStore) Close() error {
+	return b.conn.Close()
+}
+
+// FirstIndex returns the first known index from the Raft log.
+func (b *BadgerStore) FirstIndex() (uint64, error) {
+	return b.firstIndex(false)
+}
+
+// LastIndex returns the last known index from the Raft log.
+func (b *BadgerStore) LastIndex() (uint64, error) {
+	return b.firstIndex(true)
+}
+
+func (b *BadgerStore) firstIndex(reverse bool) (uint64, error) {
+	var value uint64
+	err := b.conn.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.IteratorOptions{
+			PrefetchValues: false,
+			Reverse:        reverse,
+		})
+		defer it.Close()
+
+		it.Rewind()
+		if it.Valid() {
+			value = bytesToUint64(it.Item().Key())
+		}
+		return nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	return value, nil
+}
+
+// GetLog gets a log entry from Badger at a given index.
+func (b *BadgerStore) GetLog(index uint64, log *raft.Log) error {
+	return b.conn.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(uint64ToBytes(index))
+		if err != nil {
+			switch err {
+			case badger.ErrKeyNotFound:
+				return raft.ErrLogNotFound
+			default:
+				return err
+			}
+		}
+		val, err := item.Value()
+		if err != nil {
+			return err
+		}
+		return decodeMsgPack(val, log)
+	})
+}
+
+// StoreLog stores a single raft log.
+func (b *BadgerStore) StoreLog(log *raft.Log) error {
+	val, err := encodeMsgPack(log)
+	if err != nil {
+		return err
+	}
+	return b.conn.Update(func(txn *badger.Txn) error {
+		return txn.Set(uint64ToBytes(log.Index), val.Bytes())
+	})
+}
+
+// StoreLogs stores a set of raft logs.
+func (b *BadgerStore) StoreLogs(logs []*raft.Log) error {
+	return b.conn.Update(func(txn *badger.Txn) error {
+		for _, log := range logs {
+			key := uint64ToBytes(log.Index)
+			val, err := encodeMsgPack(log)
+			if err != nil {
+				return err
+			}
+			if err := txn.Set(key, val.Bytes()); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+// DeleteRange deletes logs within a given range inclusively.
+func (b *BadgerStore) DeleteRange(min, max uint64) error {
+	// we manage the transaction manually in order to avoid ErrTxnTooBig errors
+	txn := b.conn.NewTransaction(true)
+	it := txn.NewIterator(badger.IteratorOptions{
+		PrefetchValues: false,
+		Reverse:        false,
+	})
+
+	for it.Seek(uint64ToBytes(min)); it.Valid(); it.Next() {
+		key := make([]byte, 8)
+		it.Item().KeyCopy(key)
+		// Handle out-of-range log index
+		if bytesToUint64(key) > max {
+			break
+		}
+		// Delete in-range log index
+		if err := txn.Delete(key); err != nil {
+			if err == badger.ErrTxnTooBig {
+				it.Close()
+				err = txn.Commit(nil)
+				if err != nil {
+					return err
+				}
+				return b.DeleteRange(bytesToUint64(key), max)
+			}
+			return err
+		}
+	}
+	it.Close()
+	err := txn.Commit(nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Set is used to set a key/value set outside of the raft log.
+func (b *BadgerStore) Set(key []byte, val []byte) error {
+	return b.conn.Update(func(txn *badger.Txn) error {
+		return txn.Set(key, val)
+	})
+}
+
+// Get is used to retrieve a value from the k/v store by key
+func (b *BadgerStore) Get(key []byte) ([]byte, error) {
+	var value []byte
+	err := b.conn.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil {
+			switch err {
+			case badger.ErrKeyNotFound:
+				return ErrKeyNotFound
+			default:
+				return err
+			}
+		}
+		value, err = item.ValueCopy(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+// SetUint64 is like Set, but handles uint64 values
+func (b *BadgerStore) SetUint64(key []byte, val uint64) error {
+	return b.Set(key, uint64ToBytes(val))
+}
+
+// GetUint64 is like Get, but handles uint64 values
+func (b *BadgerStore) GetUint64(key []byte) (uint64, error) {
+	val, err := b.Get(key)
+	if err != nil {
+		return 0, err
+	}
+	return bytesToUint64(val), nil
+}
---
+++ badger_store_test.go
@@ -0,0 +1,370 @@
+/*
+   Copyright 2018 Banco Bilbao Vizcaya Argentaria, S.A.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package raftbadger
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/dgraph-io/badger"
+	"github.com/hashicorp/raft"
+)
+
+func testBadgerStore(t testing.TB) (*BadgerStore, string) {
+	path, err := ioutil.TempDir("", "raftbadger")
+	if err != nil {
+		t.Fatalf("err. %s", err)
+	}
+	os.RemoveAll(path)
+
+	// Successfully creates and returns a store
+	store, err := NewBadgerStore(path)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	return store, path
+}
+
+func testRaftLog(idx uint64, data string) *raft.Log {
+	return &raft.Log{
+		Data:  []byte(data),
+		Index: idx,
+	}
+}
+
+func TestBadgerStore_Implements(t *testing.T) {
+	var store interface{} = &BadgerStore{}
+	if _, ok := store.(raft.StableStore); !ok {
+		t.Fatalf("BadgerStore does not implement raft.StableStore")
+	}
+	if _, ok := store.(raft.LogStore); !ok {
+		t.Fatalf("BadgerStore does not implement raft.LogStore")
+	}
+}
+
+func TestBadgerOptionsReadOnly(t *testing.T) {
+	store, path := testBadgerStore(t)
+	// Create the log
+	log := &raft.Log{
+		Data:  []byte("log1"),
+		Index: 1,
+	}
+	// Attempt to store the log
+	if err := store.StoreLog(log); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	store.Close()
+
+	defaultOpts := badger.DefaultOptions
+	options := Options{
+		Path:          path,
+		BadgerOptions: &defaultOpts,
+	}
+	options.BadgerOptions.ReadOnly = true
+	roStore, err := New(options)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer roStore.Close()
+
+	result := new(raft.Log)
+	if err := roStore.GetLog(1, result); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure the log comes back to same
+	if !reflect.DeepEqual(log, result) {
+		t.Errorf("bad: %v", result)
+	}
+
+	// Attempt to store the log, should fail on a read-only store
+	err = roStore.StoreLog(log)
+	if err != badger.ErrReadOnlyTxn {
+		t.Errorf("expecting error %v, but got %v", badger.ErrReadOnlyTxn, err)
+	}
+}
+
+func TestNewBadgerStore(t *testing.T) {
+	store, path := testBadgerStore(t)
+
+	// Ensure the directory was created
+	if store.path != path {
+		t.Fatalf("unexpected file path %q", store.path)
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Close the store so we can open again
+	if err := store.Close(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure our files were created
+	opts := badger.DefaultOptions
+	opts.Dir = path
+	opts.ValueDir = path
+	db, err := badger.Open(opts)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	db.Close()
+}
+
+func TestBadgerStore_FirstIndex(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.RemoveAll(path)
+
+	// Should get 0 index on empty log
+	idx, err := store.FirstIndex()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if idx != 0 {
+		t.Fatalf("bad index: %v", idx)
+	}
+
+	// Set a mock raft log
+	logs := []*raft.Log{
+		testRaftLog(1, "log1"),
+		testRaftLog(2, "log2"),
+		testRaftLog(3, "log3"),
+	}
+	if err := store.StoreLogs(logs); err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+
+	// Fetch the first Raft index
+	idx, err = store.FirstIndex()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if idx != 1 {
+		t.Fatalf("bad index: %d", idx)
+	}
+}
+
+func TestBadgerStore_LastIndex(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.RemoveAll(path)
+
+	// Should get 0 index on empty log
+	idx, err := store.LastIndex()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if idx != 0 {
+		t.Fatalf("bad index: %v", idx)
+	}
+
+	// Set a mock raft log
+	logs := []*raft.Log{
+		testRaftLog(1, "log1"),
+		testRaftLog(2, "log2"),
+		testRaftLog(3, "log3"),
+	}
+	if err := store.StoreLogs(logs); err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+
+	// Fetch the last Raft index
+	idx, err = store.LastIndex()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if idx != 3 {
+		t.Fatalf("bad index: %d", idx)
+	}
+}
+
+func TestBadgerStore_GetLog(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.RemoveAll(path)
+
+	log := new(raft.Log)
+
+	// Should return an error on non-existent log
+	if err := store.GetLog(1, log); err != raft.ErrLogNotFound {
+		t.Fatalf("expected raft log not found error, got: %v", err)
+	}
+
+	// Set a mock raft log
+	logs := []*raft.Log{
+		testRaftLog(1, "log1"),
+		testRaftLog(2, "log2"),
+		testRaftLog(3, "log3"),
+	}
+	if err := store.StoreLogs(logs); err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+
+	// Should return the proper log
+	if err := store.GetLog(2, log); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if !reflect.DeepEqual(log, logs[1]) {
+		t.Fatalf("bad: %#v", log)
+	}
+}
+
+func TestBadgerStore_SetLog(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.Remove(path)
+
+	// Create the log
+	log := &raft.Log{
+		Data:  []byte("log1"),
+		Index: 1,
+	}
+
+	// Attempt to store the log
+	if err := store.StoreLog(log); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Retrieve the log again
+	result := new(raft.Log)
+	if err := store.GetLog(1, result); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure the log comes back the same
+	if !reflect.DeepEqual(log, result) {
+		t.Fatalf("bad: %v", result)
+	}
+}
+
+func TestBadgerStore_SetLogs(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.Remove(path)
+
+	// Create a set of logs
+	logs := []*raft.Log{
+		testRaftLog(1, "log1"),
+		testRaftLog(2, "log2"),
+	}
+
+	// Attempt to store the logs
+	if err := store.StoreLogs(logs); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure we stored them all
+	result1, result2 := new(raft.Log), new(raft.Log)
+	if err := store.GetLog(1, result1); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if !reflect.DeepEqual(logs[0], result1) {
+		t.Fatalf("bad: %#v", result1)
+	}
+	if err := store.GetLog(2, result2); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if !reflect.DeepEqual(logs[1], result2) {
+		t.Fatalf("bad: %#v", result2)
+	}
+}
+
+func TestBadgerStore_DeleteRange(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.Remove(path)
+
+	// Create a set of logs
+	log1 := testRaftLog(1, "log1")
+	log2 := testRaftLog(2, "log2")
+	log3 := testRaftLog(3, "log3")
+	logs := []*raft.Log{log1, log2, log3}
+
+	// Attempt to store the logs
+	if err := store.StoreLogs(logs); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Attempt to delete a range of logs
+	if err := store.DeleteRange(1, 2); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure the logs were deleted
+	if err := store.GetLog(1, new(raft.Log)); err != raft.ErrLogNotFound {
+		t.Fatalf("should have deleted log1")
+	}
+	if err := store.GetLog(2, new(raft.Log)); err != raft.ErrLogNotFound {
+		t.Fatalf("should have deleted log2")
+	}
+}
+
+func TestBadgerStore_Set_Get(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.Remove(path)
+
+	// Returns error on non-existent key
+	if _, err := store.Get([]byte("bad")); err != ErrKeyNotFound {
+		t.Fatalf("expected not found error, got: %q", err)
+	}
+
+	k, v := []byte("hello"), []byte("world")
+
+	// Try to set a k/v pair
+	if err := store.Set(k, v); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Try to read it back
+	val, err := store.Get(k)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if !bytes.Equal(val, v) {
+		t.Fatalf("bad: %v", val)
+	}
+}
+
+func TestBadgerStore_SetUint64_GetUint64(t *testing.T) {
+	store, path := testBadgerStore(t)
+	defer store.Close()
+	defer os.Remove(path)
+
+	// Returns error on non-existent key
+	if _, err := store.GetUint64([]byte("bad")); err != ErrKeyNotFound {
+		t.Fatalf("expected not found error, got: %q", err)
+	}
+
+	k, v := []byte("abc"), uint64(123)
+
+	// Attempt to set the k/v pair
+	if err := store.SetUint64(k, v); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Read back the value
+	val, err := store.GetUint64(k)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if val != v {
+		t.Fatalf("bad: %v", val)
+	}
+}
